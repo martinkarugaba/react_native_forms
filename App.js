a8d96ce409e717ebafa68ffa53938f601d8a668c
Martin Karugaba <martinkarugaba21@gmail.com>
2024-03-02 16:27:34 +0300

    text input component

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  SafeAreaView,
+  TextInput,
+} from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput style={styles.input} value={name} onChangeText={setName} />
+      <Text style={styles.text}>My name is {name}</Text>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +22,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    padding: 10,
+    borderWidth: 2,
+  },
+  text: {
+    fontSize: 24,
+    textAlign: 'center',
   },
 });
