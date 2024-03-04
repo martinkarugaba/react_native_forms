eafa3f232f253aea2c9d7aea457f700890175951
Martin Karugaba <martinkarugaba21@gmail.com>
2024-03-04 12:35:23 +0300

    Add switch component

--- App.js
+++ App.js
@@ -6,10 +6,13 @@ import {
   StatusBar,
   SafeAreaView,
   TextInput,
+  Switch,
 } from 'react-native';
 
 export default function App() {
   const [name, setName] = useState('');
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
     <SafeAreaView style={styles.container}>
       <TextInput
@@ -22,7 +25,23 @@ export default function App() {
         // secureTextEntry
         // keyboardType="numeric"
       />
+      <TextInput
+        style={[styles.input, styles.multilineTextInput]}
+        placeholder="message"
+        multiline
+      />
+
       <Text style={styles.text}>My name is {name}</Text>
+
+      <View style={styles.switchContainer}>
+        <Text style={styles.text}>Dark Mode</Text>
+        <Switch
+          value={isDarkMode}
+          onValueChange={() => setIsDarkMode((prev) => !prev)}
+          trackColor={{ false: '#767577', true: '#81b0ff' }}
+          thumbColor="#f4f3f4"
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -38,9 +57,20 @@ const styles = StyleSheet.create({
     margin: 12,
     padding: 10,
     borderWidth: 2,
+    width: 400,
   },
   text: {
     fontSize: 24,
     textAlign: 'center',
+  },
+  multilineTextInput: {
+    minHeight: 100,
+    textAlignVertical: 'top',
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 50,
   },
 });
